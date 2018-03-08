5b9b56ae08f044c11963be549b7a03a52a806b9f
jarone <jarone@vip.qq.com>
2018-03-08 17:42:02 +0800

    add nextBigger

---
+++ nextBigger.js
@@ -0,0 +1,26 @@
+/**
+ * 函数nextBigger接收n，返回比n大的数字
+ * 返回值和n都为整型数字，且包含相同的数字
+ * 返回值和n之间不能再有比n大的数字
+ */
+
+function nextBigger(n){
+ // TODO
+}
+
+/*
+nextBigger(12) === 21;
+nextBigger(513) === 531;
+nextBigger(2017) === 2071;
+nextBigger(9) === -1;
+nextBigger(111) === -1;
+nextBigger(531) === -1;
+nextBigger(414) === 441;
+nextBigger(144) === 414;
+nextBigger(123456789) === 123456798;
+nextBigger(1234567890) === 1234567908;
+nextBigger(9876543210) === -1;
+nextBigger(9999999999) === -1;
+nextBigger(59884848459853) = 59884848483559;
+nextBigger(33399308) === 33399380;
+*/
